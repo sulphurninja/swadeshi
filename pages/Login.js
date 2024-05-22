dfcc31ea93cfc92a6740ccfdc9768e2bc433fff7
sulphurninja <sulphurninja@gmail.com>
2024-05-22 13:21:49 +0530

    Added forms ui

--- pages/Login.js
+++ pages/Login.js
@@ -1,5 +1,5 @@
 import React from 'react'
-import Layout from '../../components/globals/Layout'
+import Layout from '@/components/globals/Layout'
 import { SigninForm } from '@/components/component/signin-form'
 export default function Dashboard() {
   return (
